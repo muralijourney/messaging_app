adbc2ea2607edf021b4d1d169320c8bd29cdebe7
Mukesh Jha <mukesh.jha@journey.com>
2021-10-29 19:56:49 +0530

    ui issue fix

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-console.disableYellowBox = true;
+LogBox.ignoreLogs(['Warning: ...']);
 AppRegistry.registerComponent(appName, () => App);
